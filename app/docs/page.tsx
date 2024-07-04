f3d737ee84898175c80531e880ba29a09ef85931
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-04 18:19:53 +0700

    feat(docs): update ui

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,7 +1,15 @@
+import { ChevronRightIcon } from '@radix-ui/react-icons'
+
 export default async function Docs() {
   return (
-    <div>
-      <h1>Docs</h1>
-    </div>
+    <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div className="mx-auto w-full min-w-0">
+        <div className="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+          <div className="truncate">Docs</div>
+          <ChevronRightIcon className="size-4" />
+          <div className="font-medium text-foreground">Introduction</div>
+        </div>
+      </div>
+    </main>
   )
 }
