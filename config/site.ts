85651ecbd9b0f9c8093f05c88f7f24143cc89562
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-06 22:03:43 +0700

    docs(config): update site description

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: 'Papertrail',
   url: 'https://papertrail.pungrumpy.com',
   description:
-    'APIs for personal projects, open-source projects, and more. Papertrail is a collection of APIs that power UI.shadcn.com and other projects.',
+    'APIs for personal projects, open-source initiatives, and more. Papertrail powers cv.pungrumpy.com and other projects.',
   links: {
     github: 'https://github.com/PunGrumpy/papertrail',
     personal: 'https://pungrumpy.com'
