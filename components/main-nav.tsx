565f363f33dbced740fc9c5c6d6132d7062920a7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-06 21:57:30 +0700

    feat(header): change to use logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,24 +1,27 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useTheme } from 'next-themes'
 import * as React from 'react'
 
-import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
-
-import { Icons } from './icons'
 
 export function MainNav() {
   const pathname = usePathname()
+  const { theme, systemTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme || 'light' : theme
 
   return (
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
-        <Icons.logo className="size-6 fill-current" />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
+        <Image
+          src={currentTheme === 'dark' ? '/logo-dark.png' : '/logo-light.png'}
+          alt="Papertrail"
+          width={128}
+          height={128}
+        />
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
         <Link
