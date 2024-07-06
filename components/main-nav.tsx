5cbb2beeac4c89fcc2d2f76d31cb533f29fde250
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-07 01:02:15 +0700

    feat(logo): remove png and use svg for better exp

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,27 +1,20 @@
 'use client'
 
-import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useTheme } from 'next-themes'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
+import { Icons } from './icons'
+
 export function MainNav() {
   const pathname = usePathname()
-  const { theme, systemTheme } = useTheme()
-  const currentTheme = theme === 'system' ? systemTheme || 'light' : theme
 
   return (
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
-        <Image
-          src={currentTheme === 'dark' ? '/logo-dark.png' : '/logo-light.png'}
-          alt="Papertrail"
-          width={128}
-          height={128}
-        />
+        <Icons.logo className="size-32" />
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
         <Link
