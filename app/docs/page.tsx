2aa51bcdc05ae4db26423ccaa31943c46637206b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-08 22:23:14 +0700

    feat(docs): if not logged in redirect

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,6 +1,13 @@
 import { ChevronRightIcon } from '@radix-ui/react-icons'
+import { redirect } from 'next/navigation'
+
+import { getLoggedInUser } from '@/lib/appwrite/server'
 
 export default async function Docs() {
+  const isLoggedIn = await getLoggedInUser()
+
+  if (!isLoggedIn) redirect('/sign-in')
+
   return (
     <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
       <div className="mx-auto w-full min-w-0">
