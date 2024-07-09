ae87730f4271b043550df7d389856fe02a97e786
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-09 23:53:50 +0700

    refactor(docs): remove check logged in user to layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,12 +1,19 @@
+import { redirect } from 'next/navigation'
+
 import { DocsSidebarNav } from '@/components/nav/sidebar-nav'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { docsConfig } from '@/config/docs'
+import { getLoggedInUser } from '@/lib/appwrite/server'
 
 interface DocsLayoutProps {
   children: React.ReactNode
 }
 
-export default function DocsLayout({ children }: DocsLayoutProps) {
+export default async function DocsLayout({ children }: DocsLayoutProps) {
+  const isLoggedIn = await getLoggedInUser()
+
+  if (!isLoggedIn) redirect('/signin')
+
   return (
     <div className="border-b">
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
