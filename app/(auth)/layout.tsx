e8a2e4dfe9026c619e4248d0f2660e5fbcff9cc3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-12 00:12:41 +0700

    feat(auth): if user logged in redirect

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { redirect } from 'next/navigation'
+
+import { getLoggedInUser } from '@/lib/appwrite/server'
+
 interface AuthLayoutProps {
   children: React.ReactNode
 }
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+  const isLoggedIn = await getLoggedInUser()
+  if (isLoggedIn) redirect('/docs')
+
   return <div className="min-h-screen">{children}</div>
 }
