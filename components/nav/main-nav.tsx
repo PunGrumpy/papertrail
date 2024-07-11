e0ea6379e7a36440ce868e37efa6c18906213c56
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-11 22:32:52 +0700

    chore(icons): change logo size

--- components/nav/main-nav.tsx
+++ components/nav/main-nav.tsx
@@ -14,7 +14,7 @@ export function MainNav() {
   return (
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
-        <Icons.logo className="size-32" />
+        <Icons.logo />
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
         <Link
