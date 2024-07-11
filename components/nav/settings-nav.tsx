342b6cc457b089e30fc76f9d11eaef22a6a4221f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-11 23:29:44 +0700

    refactor(settings-nav): using scroll area for scrollable on mobile

--- components/nav/settings-nav.tsx
+++ components/nav/settings-nav.tsx
@@ -7,33 +7,37 @@ import { cn } from '@/lib/utils'
 import { SettingsNavItem } from '@/types/nav'
 
 import { buttonVariants } from '../ui/button'
+import { ScrollArea, ScrollBar } from '../ui/scroll-area'
 
 export function SettingsNav({ className, items, ...props }: SettingsNavItem) {
   const pathname = usePathname()
 
   return (
-    <nav
-      className={cn(
-        'flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1',
-        className
-      )}
-      {...props}
-    >
-      {items.map(item => (
-        <Link
-          key={item.href}
-          href={item.href || '/'}
-          className={cn(
-            buttonVariants({ variant: 'ghost' }),
-            pathname === item.href
-              ? 'bg-muted hover:bg-muted'
-              : 'hover:bg-transparent hover:underline',
-            'justify-start'
-          )}
-        >
-          {item.title}
-        </Link>
-      ))}
-    </nav>
+    <ScrollArea>
+      <div
+        className={cn(
+          'flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1',
+          className
+        )}
+        {...props}
+      >
+        {items.map(item => (
+          <Link
+            key={item.href}
+            href={item.href || '/'}
+            className={cn(
+              buttonVariants({ variant: 'ghost' }),
+              pathname === item.href
+                ? 'bg-muted hover:bg-muted'
+                : 'hover:bg-transparent hover:underline',
+              'justify-start'
+            )}
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <ScrollBar orientation="horizontal" className="invisible" />
+    </ScrollArea>
   )
 }
