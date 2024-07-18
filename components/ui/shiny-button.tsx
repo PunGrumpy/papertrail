34bfd8e60c4c571a88e9fe46a24273f7efc540e7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-18 23:50:53 +0700

    fix: add bg color

--- components/ui/shiny-button.tsx
+++ components/ui/shiny-button.tsx
@@ -27,7 +27,7 @@ const ShinyButton = ({ text = 'shiny-button' }) => {
   return (
     <motion.button
       {...animationProps}
-      className="relative h-9 rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]"
+      className="relative h-9 rounded-lg bg-background/75 px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]"
     >
       <span
         className="relative block size-full text-sm uppercase tracking-wide text-[rgb(0,0,0,65%)] dark:font-light dark:text-[rgb(255,255,255,90%)]"
