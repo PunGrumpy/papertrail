c96ccf2dcb479317a0c94b5b969389fe1526a45a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-18 23:47:00 +0700

    style(formatting): auto fixing

--- components/section/hero-section.tsx
+++ components/section/hero-section.tsx
@@ -15,20 +15,20 @@ export function HeroSection() {
       id="hero"
       className="relative mx-auto mt-32 max-w-7xl px-6 text-center md:px-8"
     >
-      <div className="animate-fade-in inline-flex h-6 items-center opacity-0 backdrop-blur transition-all ease-in">
+      <div className="inline-flex h-6 animate-fade-in items-center opacity-0 backdrop-blur transition-all ease-in">
         <Announcement />
       </div>
-      <h1 className="animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-primary from-30% to-primary/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-6xl md:text-7xl lg:text-8xl">
+      <h1 className="-translate-y-4 animate-fade-in text-balance bg-gradient-to-br from-primary from-30% to-primary/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-6xl md:text-7xl lg:text-8xl">
         Innovative Personal Projects Showcase
       </h1>
-      <p className="animate-fade-in mb-12 -translate-y-4 text-balance text-lg tracking-tight text-muted-foreground opacity-0 [--animation-delay:400ms] md:text-xl">
+      <p className="mb-12 -translate-y-4 animate-fade-in text-balance text-lg tracking-tight text-muted-foreground opacity-0 [--animation-delay:400ms] md:text-xl">
         Explore a diverse portfolio of cutting-edge projects I&apos;ve crafted
         over the years.
         <br className="hidden md:block" />
         From powerful open-source libraries to feature-rich applications,
         discover my passion for innovation.
       </p>
-      <div className="animate-fade-in -translate-y-4 space-x-2 opacity-0 [--animation-delay:600ms]">
+      <div className="-translate-y-4 animate-fade-in space-x-2 opacity-0 [--animation-delay:600ms]">
         <Link href="/docs" className={cn(buttonVariants())}>
           Explore Projects
         </Link>
