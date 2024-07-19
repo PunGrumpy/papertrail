9fce42f2cf12e72d7d318b720011c51c33c07dfc
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-20 01:17:34 +0700

    fix: wrong description

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: 'Papertrail',
   url: 'https://papertrail.pungrumpy.com',
   description:
-    'APIs for personal projects, open-source initiatives, and more. Papertrail powers cv.pungrumpy.com and other projects.',
+    "Innovative Personal Projects Showcase. Explore a diverse portfolio of cutting-edge projects I've crafted over the years. From powerful open-source libraries to feature-rich applications, discover my passion for innovation.",
   links: {
     github: 'https://github.com/PunGrumpy/papertrail',
     personal: 'https://pungrumpy.com'
